86b3beba80862114dc68ca99123fa269ef882718
Andy van Stokkum <andyvans@gmail.com>
2026-04-25 23:12:15 +1200

    update pins for S3

--- src/constants.h
+++ src/constants.h
@@ -1,15 +1,15 @@
 // I2S audio out pins
-#define I2S_LRC_OUT 12
-#define I2S_DATA_OUT 14
-#define I2S_BCLK_OUT 13
+#define I2S_LRC_OUT 8       //12
+#define I2S_DATA_OUT 18     //14
+#define I2S_BCLK_OUT 17     //13
 
 // Rotary encoder pins
-#define ENCODER_PIN_A 4
-#define ENCODER_PIN_B 5
-#define ENCODER_PIN_SWITCH 6
+#define ENCODER_PIN_A 46        // 4
+#define ENCODER_PIN_B 3         // 5
+#define ENCODER_PIN_SWITCH 9    // 6
 
 // LED matrix
-#define LED_MATRIX_PIN 7
+#define LED_MATRIX_PIN 10   // 7
 
 // Amp bluetooth on/off pin (high = on)
-#define AMP_BLUETOOTH_PIN 15                                                                  
+#define AMP_BLUETOOTH_PIN 0 // 15                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
