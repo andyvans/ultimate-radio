9dc4f7e63a777dc7be1303824180510133498005
Andy van Stokkum <andyvans@gmail.com>
2026-08-08 16:08:40 +1200

    update pins

--- src/constants.h
+++ src/constants.h
@@ -1,15 +1,15 @@
-// I2S audio out pins
+// I2S audio out pins for esp32-s3. Commented out pins are for esp32
 #define I2S_LRC_OUT 8       //12
 #define I2S_DATA_OUT 18     //14
 #define I2S_BCLK_OUT 17     //13
 
 // Rotary encoder pins
-#define ENCODER_PIN_A 46        // 4
-#define ENCODER_PIN_B 3         // 5
-#define ENCODER_PIN_SWITCH 9    // 6
+#define ENCODER_PIN_A 3        // 4
+#define ENCODER_PIN_B 46       // 5
+#define ENCODER_PIN_SWITCH 9   // 6
 
 // LED matrix
-#define LED_MATRIX_PIN 10   // 7
+#define LED_MATRIX_PIN 10      // 7
 
 // Amp bluetooth on/off pin (high = on)
-#define AMP_BLUETOOTH_PIN 48 // 15                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
+#define AMP_BLUETOOTH_PIN 48   // 15                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
